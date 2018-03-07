9db9fb1da63ed667cdba528e573b77ce88697a04
Yoza <kavan15@op.pl>
2018-03-07 17:54:25 +0100

    sc

---
+++ Zaj1/potwierdzenie.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Formularz Kontaktowy - YozaWebTech</title>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+
+<body>
+<div id="formularz">
+<p>Twoja wiadomość została wysłana.</p>
+
+
+</div>
+</body>
+
+</html>
